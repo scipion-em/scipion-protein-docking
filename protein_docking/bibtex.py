e964807bced961ecf447cd29554decd257fd2355
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2021-05-10 12:18:45 +0200

    * Creating a define binary method

--- protein_docking/bibtex.py
+++ protein_docking/bibtex.py
@@ -1,7 +1,7 @@
 # **************************************************************************
 # *
 # * Authors: Yunior C. Fonseca Reyna    (cfonseca@cnb.csic.es)
-# *          Erney Ramirez Aportela     (eramirez@cnb.csic.es)
+# *
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
@@ -25,40 +25,3 @@
 # *
 # **************************************************************************
 
-import os
-from pyworkflow.utils import Environ
-import pwem
-from .constants import PROTEIN_DOCKING_HOME
-
-_logo = ""
-_references = ['']
-__version__ = '0.0.1'
-
-
-class Plugin(pwem.Plugin):
-    _url = "https://github.com/scipion-em/scipion-protein-docking"
-    _homeVar = PROTEIN_DOCKING_HOME
-    _pathVars = [PROTEIN_DOCKING_HOME]
-    @classmethod
-    def _defineVariables(cls):
-        cls._defineEmVar(PROTEIN_DOCKING_HOME, 'frodock3-linux64')
-
-    @classmethod
-    def getEnviron(cls):
-        """ Setup the environment variables needed to launch frodock3. """
-        environ = Environ(os.environ)
-
-        environ.update({
-            'PATH': Plugin.getHome(),
-            'LD_LIBRARY_PATH': (str.join(cls.getHome(), 'lib') + ":" +
-                                cls.getHome()),
-        }, position=Environ.BEGIN)
-
-        return environ
-
-    @classmethod
-    def defineBinaries(cls, env):
-        # Add frodock3
-        env.addPackage('frodock3', version='3.12',
-                       tar='frodock3_linux64.tgz',
-                       default=True)
